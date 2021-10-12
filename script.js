949e75e9399a7298e72ff5362aae90cf69ad124b
Sangeetha Kumarasamy <89238309+SangeethaKumarasamy@users.noreply.github.com>
2021-10-12 11:05:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function newList() {
+    var select = document.getElementById("list");
+    var newOption = document.createElement("option");
+    
+    newOption.text = document.getElementById("txtbox").value;
+    select.add(newOption);
+  }
+  
